af4f82e6ddc77ffa17cda1eff65a6f84678bfee7
MaxB <maximebouchard2002@hotmail.com>
2023-12-04 22:46:59 -0500

    makui

--- aoc1/aoc1.py
+++ aoc1/aoc1.py
@@ -1,17 +1,17 @@
-ins = [lines for lines in open("aoc1/agherjiolghyhny.txt", "r").read().split("\n")]
-fnb = 0
-dick = {"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9}
-for l in ins:
-    tnb = ()
-    s = ""
-    for c in l:
-        if(c.isnumeric()):
-            tnb.append(int(c))
-            s = ""
+inp = [lines for lines in open("aoc1/agherjiolghyhny.txt", "r").read().split("\n")]
+finalnb = 0
+dic = {"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9}
+for line in inp:
+    tempnb = ()
+    stri = ""
+    for char in line:
+        if(char.isnumeric()):
+            tempnb.append(int(char))
+            stri = ""
         else:
-            s += c
-            if(dick.get(s) is not None):
-                tnb.append(dick.get(s))
-                s = ""
-    fnb += (tnb[0] * 10) + tnb[-1]
-print(fnb)
+            stri += char
+            if(dic.get(stri) is not None):
+                tempnb.append(dic.get(stri))
+                stri = ""
+    finalnb += (tempnb[0] * 10) + tempnb[-1]
+print(finalnb)
