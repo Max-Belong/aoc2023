8cd3abcf1700957f50eed4941a3d51e3e59a1188
MaxB <maximebouchard2002@hotmail.com>
2023-12-04 23:47:37 -0500

    jva laisser ça la pour asoér

--- aoc1/aoc1.py
+++ aoc1/aoc1.py
@@ -1,17 +1,24 @@
 inp = [lines for lines in open("aoc1/agherjiolghyhny.txt", "r").read().split("\n")]
 finalnb = 0
 dic = {"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9}
-for line in inp:
-    tempnb = ()
-    stri = ""
-    for char in line:
-        if(char.isnumeric()):
-            tempnb.append(int(char))
+
+def p1(inp):
+    for line in inp:
+        tempnb = []
+        for num in line:
+            if(num.isnumeric()):
+                tempnb.append(int(num))
+
+def p2(inp):
+    for char in inp:
+        tempnb = []
+        stri += char
+        if(stri.isnumeric()):
             stri = ""
-        else:
-            stri += char
-            if(dic.get(stri) is not None):
-                tempnb.append(dic.get(stri))
-                stri = ""
+        if(dic.get(stri) is not None):
+            tempnb.append(dic.get(stri))
+            stri = ""
+
+
     finalnb += (tempnb[0] * 10) + tempnb[-1]
 print(finalnb)
